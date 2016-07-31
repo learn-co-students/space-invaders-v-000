d729db7f60ef45fdc10316bc4238ad76e8604e2d
Jason Casares <jasonc@csdrafting.com>
2016-07-31 16:22:30 +0000

    Done.

--- spaceship.js
+++ spaceship.js
@@ -1,11 +1,23 @@
 class Spaceship {
-  constructor() {
-
+  constructor(name, crew, phasers, shields) {
+    this.name = name;
+    this.crew = crew;
+    this.phasers = phasers;
+    this.shields = shields;
+    this.cloaked = false;
+    this.warpDrive = 'disengaged';
+    if (crew.length === 0) {
+      this.docked = true;
+    }else{
+      this.docked = false;
+    }
+    this.phasersCharge = 'uncharged';
+    this.notifyCrew();
   }
-}
 
-class CrewMember {
-  constructor() {
-
+  notifyCrew(){
+    this.crew.forEach(crewMember => {
+      crewMember.currentShip = this;
+    });
   }
 }
