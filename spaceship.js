40eaaccea777bcbe6707756747331e1a738fe306
michaelsimon <michael.simon@gmail.com>
2016-08-14 13:20:43 -0400

    Done.

---
+++ spaceship.js
@@ -0,0 +1,20 @@
+class Spaceship {
+  constructor(name, crew, phasers, shields) {
+    this.name = name;
+    this.crew = crew;
+    this.phasers = phasers;
+    this.shields = shields;
+    this.cloaked = false;
+    this.warpDrive ='disengaged';
+    if (crew.length == 0) {
+      this.docked = true;
+    }
+    else {
+      this.docked = false;
+      for (var i= 0; i < (crew.length); i++) {
+        crew[i].currentShip = this;
+      }
+    }
+    this.phasersCharge = 'uncharged';
+  }
+}
