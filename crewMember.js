2d92cf80fbc1e443e038caec0b9b2a8757c1c839
azemoh <joshazemoh@gmail.com>
2016-09-09 00:18:08 +0100

    Done.

--- crewMember.js
+++ crewMember.js
@@ -1,1 +1,35 @@
+class CrewMember {
 
+  constructor(position) {
+    this.position = position
+    this.currentShip = 'Looking for a Rig'
+  }
+
+  engageWarpDrive() {
+    if (this.position === 'Pilot' && this.hasShip()) {
+      this.currentShip.warpDrive = 'engaged!'
+    }
+
+    return 'had no effect'
+  }
+
+  setsInvisibility() {
+    if (this.position === 'Defender' && this.hasShip()) {
+      this.currentShip.cloaked = true
+    }
+
+    return 'had no effect'
+  }
+
+  chargePhasers() {
+    if (this.position === 'Gunner' && this.hasShip()) {
+      this.currentShip.phasersCharge = 'charged!'
+    }
+
+    return 'had no effect'
+  }
+
+  hasShip() {
+    return this.currentShip !== 'Looking for a Rig'
+  }
+}
