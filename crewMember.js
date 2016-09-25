14e4079fdd2a25a0b8397264e50c79d4ad4833fc
Megan Eloise <mev277@nyu.edu>
2016-09-25 14:16:11 +0200

    Done.

--- crewMember.js
+++ crewMember.js
@@ -1,1 +1,19 @@
-
+class Spaceship {
+  constructor(name, crew, phasers, shields, cloaked = false, warpDrive = "disengaged") {
+    this.name = name;
+    this.crew = crew
+    this.phasers = phasers
+    this.shields = shields
+    this.cloaked = cloaked
+    this.warpDrive = warpDrive
+    if (crew.length == 0) {
+      this.docked = true
+    } else {
+      this.docked = false
+    }
+    this.phasersCharge = 'uncharged'
+    this.crew.forEach(member => {
+      member.currentShip = this
+    })
+  }
+}
