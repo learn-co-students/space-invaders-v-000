0622bb3173eed5d624190c1ef3f72c29fd533862
Christian Whitesides <whitesidescr@gmail.com>
2016-10-27 16:44:22 -0600

    Done.

---
+++ spaceship.js
@@ -0,0 +1,18 @@
+class Spaceship {
+  constructor(name, crew, phasers, shields) {
+    this.name = name;
+    this.crew = crew;
+    this.docked = true;
+    this.phasers = phasers;
+    this.shields = shields;
+    this.cloaked = false;
+    this.warpDrive = "disengaged";
+    this.phasersCharge = "uncharged";
+    if (crew.length > 0) {
+      this.docked = false;
+      for (let i = 0; i < crew.length; i++) {
+        crew[i].currentShip = this;
+      }
+    }
+  }
+}
