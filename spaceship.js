bd35faa5764f6d42c86334255f783a6f3cc5d98b
jchu4483 <jchu4483@gmail.com>
2016-11-01 21:10:03 -0400

    Done.

---
+++ spaceship.js
@@ -0,0 +1,25 @@
+class Spaceship {
+  constructor(name, crew, phasers, shields) {
+    this.name = name;
+    this.crew = crew;
+    this.phasers = phasers;
+    this.shields = shields;
+    this.cloaked = false;
+    this.warpDrive = 'disengaged';
+    var dock;
+    if (crew.length === 0) {
+      dock = true;
+    } else {
+      dock = false;
+    }
+    this.docked = dock;
+    this.phasersCharge = 'uncharged';
+    this.notifyCrew();
+  }
+  notifyCrew() {
+    this.crew.forEach(crewMember => {
+      crewMember.currentShip = this;
+    });
+  }
+
+}
