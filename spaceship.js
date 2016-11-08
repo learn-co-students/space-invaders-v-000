08c3140075282fe9aedc9ca1f1b5cba7f9a9a442
tpstar <hanlee12@gmail.com>
2016-11-08 03:15:08 +0000

    done

--- spaceship.js
+++ spaceship.js
@@ -1,16 +1,49 @@
-
 class CrewMember {
   constructor(position) {
     this.position = position;
-    this.currentShip = 'Looking for a Rig'
+    if (this.currentShip === undefined) {
+    	this.currentShip = "Looking for a Rig";
+    }
   }
+  engageWarpDrive() {
+    	if (this.currentShip === "Looking for a Rig") {
+    		return 'had no effect';
+    	} else if (this.position === "Pilot") {
+    		this.currentShip.warpDrive = 'engaged!';
+    	}
+    }
+    setsInvisibility() {
+    	if (this.currentShip === "Looking for a Rig") {
+    		return 'had no effect';
+    	} else if (this.position === "Defender") {
+    		this.currentShip.cloaked = true;
+    	}
+    }
+
+    chargePhasers() {
+      if (this.currentShip === "Looking for a Rig") {
+    		return 'had no effect';
+    	} else if (this.position === "Gunner") {
+    		this.currentShip.phasersCharge = 'charged!';
+    	}
+    }
 }
 
 class Spaceship {
   constructor(name, crews, phasers, shields) {
     this.name = name;
-    this.crews = crews;
     this.phasers = phasers;
     this.shields = shields;
+    this.cloaked = false;
+    this.warpDrive = 'disengaged';
+    this.phasersCharge = 'uncharged';
+    if (crews.length === 0) {
+      this.docked = true;
+    } else {
+      this.docked = false;
+    }
+    for(var i = 0, len = crews.length; i < len; i ++) {
+     crews[i]['currentShip'] = this;
+  	}
   }
 }
