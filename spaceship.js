b49d7f41e832625906f5fb653a13996404977c75
Laurent Milne <lmilne360@gmail.com>
2017-01-13 04:53:41 +0000

    Done.

---
+++ spaceship.js
@@ -0,0 +1,27 @@
+class Spaceship{
+  constructor(name, crew, phasers, shields){
+    this.name = name;
+    this.crew = crew;
+    this.phasers = phasers;
+    this.shields = shields;
+    this.cloaked = false;
+    this.warpDrive = "disengaged";
+    this.phasersCharge = "uncharged";
+    if (crew.length === 0){
+      this.docked = true;
+    }else{
+      this.docked = false;
+      this.recruitMembers();
+    }
+
+  }
+
+  recruitMembers(){
+    var ship = this;
+    this.crew.forEach(function(member) {
+      member.currentShip = ship;
+    });
+  }
+
+
+}
