dbd4ac7089fae1e46f547dbe7a7d92a3c2c78f29
Villy Siu <villysiu@gmail.com>
2017-01-21 20:00:59 +0000

    Done.

---
+++ spaceship.js
@@ -0,0 +1,21 @@
+function Spaceship (name, crew, phasers, shields) {
+
+    this.name = name;
+    this.crew = crew;
+    this.phasers = phasers;
+    this.shields = shields;
+    this.cloaked = false;
+    this.warpDrive = 'disengaged';
+    this.phasersCharge = 'uncharged'
+
+    if (crew.length == 0){
+      this.docked = true;
+    }
+    else {
+      this.docked = false;
+      for (var i=0; i<crew.length; i++) {
+        crew[i].currentShip = this;
+      }
+    }
+
+}
