122ef2715991e6b46b7408138f4dcee33b443fcf
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-04-14 23:42:28 +0000

    Done.

---
+++ spaceship.js
@@ -0,0 +1,23 @@
+class Spaceship {
+  constructor(name, crewMembers, phasers, shields) {
+    this.name = name 
+    this.crewMembers = crewMembers
+    this.phasers = phasers 
+    this.shields = shields 
+    this.cloaked = false
+    this.warpDrive = "disengaged"
+    this.phasersCharge = "uncharged"
+    
+    if (this.crewMembers.length === 0) {
+      this.docked = true 
+    } else {
+      this.docked = false
+    }
+
+    crewMembers.forEach( i => {
+      i.currentShip = this
+    })
+    
+  }
+
+}
