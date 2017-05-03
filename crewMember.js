d9443db838ce627f97a5b9e1588757c0d3c811f3
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-05-03 11:39:55 -0400

    Done.

--- crewMember.js
+++ crewMember.js
@@ -1,1 +1,33 @@
-
+class CrewMember{
+    constructor(position){
+        this.position = position;
+        this.currentShip = 'Looking for a Rig'
+    }
+    engageWarpDrive(){
+        if (this.position === 'Pilot' && this.currentShip != 'Looking for a Rig'){
+          console.log(`Engaging warp drive of ${this.currentShip.name}`)
+            this.currentShip.warpDrive = 'engaged!'
+        } else {
+            return "had no effect"
+        }
+    }
+    setsInvisibility(){
+        if (this.position === "Defender" && this.currentShip != 'Looking for a Rig'){
+          console.log(`Setting invisibility of ${this.currentShip.name}`)
+            this.currentShip.cloaked = true;
+        } else {
+            return "had no effect"
+        }
+    }
+    chargePhasers(){
+        if (this.position === "Gunner" && this.currentShip != 'Looking for a Rig'){
+          console.log(`Chargin phasers of ${this.currentShip.name}`)
+            this.currentShip.phasersCharge = 'charged!'
+        } else {
+            return "had no effect"
+        }
+    }
+    setCurrentShip(ship){
+        this.currentShip = ship;
+    }
+}
