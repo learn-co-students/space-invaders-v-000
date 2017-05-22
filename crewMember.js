61ad65cc01bc465724e90b82ca120b87b4c7c444
Malind Kowalski <malind.nikulski@gmail.com>
2017-05-22 14:54:11 -0600

    finally completed space lab!!

--- crewMember.js
+++ crewMember.js
@@ -1,1 +1,30 @@
+class CrewMember {
+  constructor(position) {
+    this.position = position;
+    this.currentShip = 'Looking for a Rig';
+  }
 
+  useAbility(position) {
+    if (this.position === position && !(this.currentShip === 'Looking for a Rig')) {
+      const ability = {
+        'Pilot':    'engageWarp',
+        'Defender': 'cloak',
+        'Gunner':   'arm'
+      }[position];
+      return this.currentShip[ability]();
+    }
+    return 'had no effect';
+  }
+
+  engageWarpDrive() {
+    return this.useAbility('Pilot');
+  }
+
+  setsInvisibility() {
+    return this.useAbility('Defender');
+  }
+
+  chargePhasers() {
+    return this.useAbility('Gunner');
+  }
+}
