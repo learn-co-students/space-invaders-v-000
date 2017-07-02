83df844c2b28338103281219fc8640e97d869d62
Patrick Sattelberger <sattelbergerp@gmail.com>
2017-07-02 12:16:12 -0400

    Done.

--- crewMember.js
+++ crewMember.js
@@ -1,1 +1,36 @@
+class CrewMember{
 
+  constructor(position){
+    this.position=position
+    this.currentShip = 'Looking for a Rig'
+  }
+
+  hasShip(){
+    return this.currentShip != 'Looking for a Rig'
+  }
+
+  engageWarpDrive(){
+    if(this.hasShip()&&this.position=='Pilot'){
+      this.currentShip.warpDrive = 'engaged!';
+    }else{
+      return 'had no effect';
+    }
+  }
+
+  setsInvisibility(){
+    if(this.hasShip()&&this.position=='Defender'){
+      this.currentShip.cloaked = true;
+    }else{
+      return 'had no effect';
+    }
+  }
+
+  chargePhasers(){
+    if(this.hasShip()&&this.position=='Gunner'){
+      this.currentShip.phasersCharge = 'charged!';
+    }else{
+      return 'had no effect';
+    }
+  }
+
+}
