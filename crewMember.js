498328cc7674b60e30b01d8c0a3314940208d396
lucketta <lucketta@umich.edu>
2017-07-25 00:43:22 -0400

    Done.

--- crewMember.js
+++ crewMember.js
@@ -1,1 +1,11 @@
+class CrewMember {
+  constructor(position){
+    this.position = position
+  }
 
+  currentShip() {
+    if (!this.currentShip){
+      return "Looking for a Rig";
+    }
+  }
+}
---
+++ spaceship.js
@@ -0,0 +1,12 @@
+class Spaceship {
+  constructor(name, crew, phasers, shields) {
+    this.name = name;
+    this.crew = crew;
+    this.phasers = phasers;
+    this.shields = shields;
+    this.cloaked = false;
+    this.warpDrive = "disengaged";
+    this.docked = true
+    this.phasersCharge = "uncharged"
+  }
+}
