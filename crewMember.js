6cf0ca9dba2dc1b6895c6859464b5ebab34dde6b
Sammy Wu <wu.sammy@gmail.com>
2017-07-25 01:58:22 +0000

    Done.

--- crewMember.js
+++ crewMember.js
@@ -1,1 +1,30 @@
-
+class CrewMember {
+  constructor(position) {
+    this.position = position;
+    this.currentShip = "Looking for a Rig";
+  }
+  // Pilots should only be able to engage warp drive if they have a ship.
+  engageWarpDrive() {
+    if (this.position === 'Pilot' && this.currentShip !== 'Looking for a Rig') {
+      this.currentShip.warpDrive = "engaged!";
+    } else {
+      return "had no effect";
+    }
+  }
+  // Defenders should only be able to cloak the ship if they have a ship.
+  setsInvisibility() {
+    if (this.position === 'Defender' && this.currentShip !== 'Looking for a Rig') {
+      this.currentShip.cloaked = true;
+    } else {
+      return "had no effect";
+    }
+  }
+  // Gunners should only be able to charge phasers if they have a ship.
+  chargePhasers() {
+    if (this.position === 'Gunner' && this.currentShip !== 'Looking for a Rig') {
+      this.currentShip.phasersCharge = "charged!";
+    } else {
+      return "had no effect";
+    }
+  }
+}
