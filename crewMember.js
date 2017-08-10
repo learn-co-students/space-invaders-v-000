b0616bbf70f3744c71bbf5ecb69060c3df7112ff
Yevgeniy Skroznikov <ohnap@hotmail.com>
2017-08-10 16:49:58 +0000

    Done.

--- crewMember.js
+++ crewMember.js
@@ -1,1 +1,31 @@
 
+class CrewMember{
+  constructor(position){
+    this.position = position
+    this.currentShip = 'Looking for a Rig'
+  }
+
+  canUseAbility(){
+    if(this.currentShip === 'Looking for a Rig')
+      return false
+    return true
+  }
+
+  engageWarpDrive(){
+    if(!this.canUseAbility() || this.position !== 'Pilot')
+      return 'had no effect'
+    this.currentShip.warpDrive = 'engaged!'
+  }
+
+  setsInvisibility(){
+    if(!this.canUseAbility() || this.position !== 'Defender')
+      return 'had no effect'
+    this.currentShip.cloaked = true
+  }
+
+  chargePhasers(){
+    if(!this.canUseAbility() || this.position !== 'Gunner')
+      return 'had no effect'
+    this.currentShip.phasersCharge = 'charged!'
+  }
+}
