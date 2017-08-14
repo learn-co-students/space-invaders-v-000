d25d7bc10517d0ef7cbed98e81887b6682092d85
FreeBreadsticks <erika.hughes@dbspublishing.com>
2017-08-14 15:42:49 -0400

    Done.

--- crewMember.js
+++ crewMember.js
@@ -1,1 +1,28 @@
+class CrewMember {
+  constructor(position) {
+    this.position = position;
+    this.currentShip = "Looking for a Rig";
+    this.chargePhasers =  ()=> {
+      if (this.currentShip != "Looking for a Rig" && this.position === "Gunner") {
+        this.currentShip.phasersCharge = "charged!";
+      } else {
+        return "had no effect";
+      }
+    }
+    this.engageWarpDrive =  ()=> {
+      if (this.currentShip != "Looking for a Rig" && this.position === "Pilot") {
+        this.currentShip.warpDrive = "engaged!";
+      } else {
+        return "had no effect";
+      }
+    }
+    this.setsInvisibility =  ()=> {
+      if (this.currentShip != "Looking for a Rig" && this.position === "Defender") {
+        this.currentShip.cloaked = true;
+      } else {
+        return "had no effect";
+      }
+    }
+  }
 
+}
