545e6efd2ed15dc051e1bb6423b84a4d8b028e67
Bruce Molina <bruce.molina.81@gmail.com>
2017-08-30 23:42:17 +0000

    Done.

--- spaceship.js
+++ spaceship.js
@@ -1,41 +1,56 @@
 class Spaceship{
-  constructor(name, crew, phasers,shields){
+  constructor(name, crew_array, phasers, shields ){
     this.name = name;
-    this.crew = crew;
+    this.crew_array = crew_array
+    this.crew_array.length === 0 ? this.docked = true : this.docked = false
+    this.crew_array = crew_array.forEach( (Crewmember) => Crewmember.currentShip = this )
     this.phasers = phasers;
+    this.phasersCharge = 'uncharged'
     this.shields = shields;
     this.warpDrive = 'disengaged';
-    this.phasersCharge = 'uncharged';
     this.cloaked = false;
-  
-  if (crew.length === 0 ){
-    this.docked = true
-  } else {
-    this.docked = false 
-  };
+  } // constructor
 
-  this.crewShip();
+
+}
+
+
+class CrewMember{
+  constructor(position){
+    this.position = position;
+    this.currentShip = "Looking for a Rig"
   }
 
-  crewShip(){
-    this.crew.forEach(crewmate => {
-      crewmate.currentShip = this;
-    });
+  chargePhasers(){
+    if (this.currentShip === "Looking for a Rig"){
+      return "had no effect"
+    }
+    if (this.position === "Gunner" && this.currentShip != "Looking for a Rig"){
+      this.currentShip.phasersCharge = "charged!"
+    } else {
+      this.currentShip.phasersCharge = "uncharged"
+    }
   }
 
+  engageWarpDrive(){
+    if (this.currentShip === "Looking for a Rig"){
+      return "had no effect"
+    }
+    if (this.position === "Pilot" && this.currentShip != "Looking for a Rig"){
+      this.currentShip.warpDrive = "engaged!"
+    } else {
+      this.currentShip.warpDrive = "disengaged"
+    }
+  }
 
-
-
-
-}  // end spaceship
-
-// class User {
-//   constructor(name, email) {
-//     this.name = name;
-//     this.email = email;
-//   }
- 
-//   sayHello() {
-//     console.log("Hello, my name is "+ this.name);
-//   }
-// }
+  setsInvisibility(){
+    if (this.currentShip === "Looking for a Rig"){
+      return "had no effect"
+    }
+    if (this.position === "Defender" && this.currentShip != "Looking for a Rig"){
+      this.currentShip.cloaked = true
+    } else {
+      this.currentShip.cloaked = false
+    }
+  }
+}
