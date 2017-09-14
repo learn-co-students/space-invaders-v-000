43dbb9183c71d13010ad2b7202aa6ea20d20ef55
curlywallst <nnoyes@comcast.net>
2017-09-14 19:25:45 -0400

    Done.

--- spaceship.js
+++ spaceship.js
@@ -1,35 +1,5 @@
-class CrewMember {
-  constructor (position) {
-    this.position = position
-    this.currentShip = "Looking for a Rig"
-  }
-  engageWarpDrive() {
-      debugger;
-    if (this.position === 'Pilot' && this.currentShip != "Looking for a Rig") {
-      this.currentShip.warpDrive = "engaged!"
-    } else {
-      return 'had no effect'
-    }
-  }
-  setsInvisibility() {
-    if (this.position === 'Defender' && this.currentShip != "Looking for a Rig") {
-      this.currentShip.cloaked = true
-    } else {
-      return 'had no effect'
-    }
-  }
-  chargePhasers() {
-    if (this.position === 'Gunner' && this.currentShip != "Looking for a Rig") {
-      this.currentShip.phasersCharge = "charged!"
-    } else {
-      return 'had no effect'
-    }
-  }
-}
-
-class Spaceship extends CrewMember {
+class Spaceship {
   constructor (name, crew, phasers, shields) {
-    super();
     this.name = name
     this.crew = crew
     this.phasers = phasers
