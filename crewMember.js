b75636ad9c2be1e1ce1ad5ddb0b58587988df7a1
github username <github email address>
2017-09-21 13:15:57 -0500

    Done.

--- crewMember.js
+++ crewMember.js
@@ -28,16 +28,3 @@ class CrewMember {
 		}
 	}
 }
-
-// class Spaceship {
-// 	constructor(name, crew, phasers, shields) {
-// 		this.name = name
-// 		this.crew = crew
-// 		this.phasers = phasers
-// 		this.shields = shields
-// 		this.cloaked = false
-// 		this.warpDrive = "disengaged"
-// 		this.docked = this.crew.length < 1 ? true : false
-// 		this.phasersCharge = "uncharged"
-// 	}
-// }
