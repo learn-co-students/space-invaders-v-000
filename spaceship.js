9397bfe4f004226122f1d2489c9c70ed1bd7132c
James Fernandez <jffernan@comcast.net>
2017-10-06 15:39:45 -0700

    Re-do & added pseudocode // comments

--- spaceship.js
+++ spaceship.js
@@ -1,3 +1,10 @@
+/* 
+1. Create two ES6 JS classes, Spaceship and CrewMember. 
+2.A space ship will be inactive if it is created without a crew. 
+3. To have these two object types be aware of one another we need to have the ship become aware of its crew members when it's created.
+4. This will also require you to make sure a crew member becomes aware of their ship when they are added to it.
+*/
+
 class Spaceship {
   constructor(name, crew, phasers, shields) {
     this.name = name;
