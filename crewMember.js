e49adb15c83d3fd1ad56e6f03a4302fedc5c6fc3
BuffaloRob <peckham.rob@gmail.com>
2018-03-07 09:18:33 -0500

    done

--- crewMember.js
+++ crewMember.js
@@ -1,1 +1,27 @@
-class CrewMember {}
+//will default to cantina, if not assigned to a crew when a ship is made. Also, can't use special ability until assigned to a ship
+class CrewMember {
+  constructor(position) {
+    this.position = position;
+    this.currentShip = "Looking for a Rig";
+  }
+
+  engageWarpDrive() {
+    return this.position === "Pilot" && this.currentShip !== "Looking for a Rig"
+      ? (this.currentShip.warpDrive = "engaged")
+      : "had no effect";
+  }
+
+  setsInvisibility() {
+    return this.position === "Defender" && this.currentShip !== "Looking for a Rig"
+    ? (this.currentShip.cloaked = true)
+    : "had no effect";
+  }
+
+  chargePhasers() {
+    return this.position === "Gunner" && this.currentShip !== "Looking for a Rig"
+    ? (this.currentShip.phasersCharge = "charged")
+    : "had no effect";
+  }
+
+  
+}
