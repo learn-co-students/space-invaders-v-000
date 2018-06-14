a5d366e6e241181130ae6e680cc4019047465f55
Jrodr4544 <maskdbandit@gmail.com>
2018-06-14 23:06:13 +0000

    Done.

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,20 @@
-class CrewMember {}
+class CrewMember {
+	
+	constructor(pos, currentShip = "Looking for a rig") {
+	 this.position    = pos;	
+	 this.currentShip = currentShip;
+	}
+
+	chargePhasers() {
+	 return (this.currentShip instanceof Spaceship && this.position == "Gunner" ? this.currentShip.phasersCharge = "charged!" : "had no effect");
+	}
+
+	engageWarpDrive() {
+	 return (this.currentShip instanceof Spaceship && this.position == "Pilot" ? this.currentShip.warpDrive = "engaged!" : "had no effect");
+	}
+
+	setsInvisibility() {
+	 return (this.currentShip instanceof Spaceship && this.position == "Defender" ? this.currentShip.cloaked = true : "had no effect");
+	}
+
+}
--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,20 @@
-class Spaceship {}
+class Spaceship {
+
+	constructor(name, crew = [], phasers = "uncharged", shields){
+
+	    for (var i = 0; i < crew.length; i++) {
+	      crew[i].currentShip = this;
+	    };	
+	 		
+	 this.name    = name;
+	 this.phasers = phasers;
+         this.shields = shields;
+	 this.crew    = crew;
+	 this.docked  = (crew.length > 0 ? false : true);
+	 this.cloaked = false; 
+	 this.warpDrive     = "disengaged";
+	 this.phasersCharge = "uncharged";
+	}
+
+
+}
